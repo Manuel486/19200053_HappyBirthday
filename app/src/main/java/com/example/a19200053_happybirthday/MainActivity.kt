3ce6df992caa1af81a231f563e2d700dda17599c
AULA F.DE ING.DE SISTEMAS E INF <AULAFISI@UNMSM.pe>
2023-08-23 21:25:09 -0500

    codlab

--- app/src/main/java/com/example/a19200053_happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/a19200053_happybirthday/MainActivity.kt
@@ -3,22 +3,29 @@ package com.example.a19200053_happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.a19200053_happybirthday.ui.theme._19200053_HappyBirthdayTheme
+import com.example.happybirthday.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,7 +37,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText(message = "Feliz Cumpleaños DSM!!!", from = "de Zoraida")
+                    // GreetingText(stringResource(R.string.happy_birthday_text), stringResource(R.string.from_txt))
+                    /*GreetingImage(message = getString(R.string.happy_birthday_text), from = getString(
+                        R.string.from_txt
+                    ))*/
+                    /*Article(title = stringResource(R.string.article_title_txt), article1 = stringResource(R.string.article_content1_txt), article2 = stringResource(
+                        R.string.article_content2_txt
+                    ) )*/
                 }
             }
         }
@@ -58,10 +71,68 @@ fun GreetingText(message: String,from: String,modifier: Modifier = Modifier) {
     }
 }
 
+
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    //Step 3 create a box to overlap image and texts
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
+}
+
+@Composable
+fun Article(title: String,article1: String, article2: String){
+    val image = painterResource(R.drawable.bg_compose_background)
+    Column {
+        Image(painter = image, contentDescription = null)
+        Text(text = title, Modifier.padding(16.dp) ,fontSize = 24.sp)
+        Text(text = article1,  Modifier.padding(16.dp), textAlign = TextAlign.Justify)
+        Text(text = article2,  Modifier.padding(16.dp), textAlign = TextAlign.Justify)
+    }
+}
+
+@Composable
+fun TaskManager(title: String,message: String){
+    val image = painterResource(R.drawable.ic_task_completed)
+    Column(  verticalArrangement = Arrangement.Center ) {
+        Image(painter = image, contentDescription = null)
+        Text(text = title,
+            Modifier
+                .padding(
+                top = 24.dp,
+                bottom = 8.dp
+                )
+                .align(alignment = Alignment.CenterHorizontally))
+
+        Text(text = message, Modifier
+            .align(alignment = Alignment.CenterHorizontally))
+    }
+}
+
+
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview(){
     _19200053_HappyBirthdayTheme {
-        GreetingText(message = "Feliz Cumpleaños DSM!!!", from = "de Zoraida")
+        // GreetingText(stringResource(R.string.happy_birthday_text), stringResource(R.string.from_txt))
+        // GreetingImage(stringResource(R.string.happy_birthday_text), stringResource(R.string.from_txt))
+        /* Article(title = stringResource(R.string.article_title_txt), article1 = stringResource(R.string.article_content1_txt), article2 = stringResource(
+            R.string.article_content2_txt
+        )) */
+        TaskManager(title = "All tasks completed", message = "Nice work!")
     }
 }
